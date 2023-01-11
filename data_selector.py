657cb1345fbd993f94bd52198a2c931dd717d47d
Sebastian Castro <jcastropa@unal.edu.co>
2023-01-11 07:56:00 -0500

    programm working

--- data_selector.py
+++ data_selector.py
@@ -1,20 +1,22 @@
 import csv
 
-path = "example.csv"
+path = "EDX_Data.csv"
 data = []
-avg = 5
 
 with open(path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        data = row
-        
-avg_list = []
+        data = row                                                  # lee y guarda todos los valores de la columna en una lista
+        avg = float(data.pop(len(data)-1))                                # elimina y establece el último valor como el promedio con el que 
+                                                                    # compararemos los datos  
+        distances = []                                              # aquí guardaremos las distancias entre cada dato y el promedio
 
-for x in data:
-    avg_list.append(abs(float(x)-avg))
+        for x in data:
+            distances.append(abs(float(x)-avg))                     # calculamos y guardamos cada distancia en la lista
     
-min_list = sorted(zip(data, avg_list), key=lambda t: t[1])
+        min_list = sorted(zip(data, distances), key=lambda t: t[1])  # aquí organizamos la lista de datos por la lista de distancias
 
-print(min_list)
-    
+        for i in range(0,5):
+            print(min_list[i][0])
+            
+        print("___________")
